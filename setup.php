6c94557f040c850c6115155744aed1f9291273ef
aboTheHecker <katugneilasher@gmail.com>
2026-05-02 14:39:26 +0800

    modified setup #2

--- setup.php
+++ setup.php
@@ -2,8 +2,11 @@
 require_once 'includes/db.php';
 
 $sql = file_get_contents('db/ecommerce.sql');
+
+$sql = preg_replace('/USE\s+\w+;/i', '', $sql);
+$sql = preg_replace('/CREATE\s+DATABASE[^;]+;/i', '', $sql);
+
 $conn->multi_query($sql);
 
-echo "Database imported successfuly";
-
+echo "Database imported successfully!";
 ?>
